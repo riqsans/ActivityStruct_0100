891b4581f7a1686b2908c6c513fd59042de46c4b
riqsans <thrq88@gmail.com>
2024-04-01 14:19:22 +0700

    cout nim mahasiswa

--- 100_Struct/100_Struct.cpp
+++ 100_Struct/100_Struct.cpp
@@ -16,4 +16,12 @@ struct Mahasiswa {
 	string umur;
 };
 
+int main() {
+	Mahasiswa mhs[3];
+	for (int i = 0;i < 3;i++) {
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor Mahasiswa : ";
+	}
+}
+
 
