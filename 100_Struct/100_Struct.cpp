84ede0f37354a37eb970cb363aeb995509347f8a
riqsans <thrq88@gmail.com>
2024-04-01 15:10:04 +0700

    membuat struct detail alamat

--- 100_Struct/100_Struct.cpp
+++ 100_Struct/100_Struct.cpp
@@ -8,31 +8,3 @@ struct DetailAlamat {
     string kota;
 };
 
-struct Mahasiswa {
-    string nim;
-    string nama;
-    DetailAlamat alamat; 
-    string umur; 
-};
-
-int main() {
-    Mahasiswa mhs[3];
-    for (int i = 0; i < 3; i++) {
-        cout << "Data ke-" << (i + 1) << ":" << endl;
-        cout << "Nomor Mahasiswa : ";
-        getline(cin, mhs[i].nim);
-        cout << "Nama Mahasiswa : ";
-        getline(cin, mhs[i].nama);
-
-        cout << "Alamat Mahasiswa : " << endl;
-        cout << "\t Nama Desa : ";
-        getline(cin, mhs[i].alamat.desa); 
-        cout << "\t Nama Kota : ";
-        getline(cin, mhs[i].alamat.kota);
-
-        cout << "Umur Mahasiswa : ";
-        getline(cin, mhs[i].umur); 
-    }
-
-    
-}
