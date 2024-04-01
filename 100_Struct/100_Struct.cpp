d1c0ba9fca7b8623e0f477b35a2a7fba04ec968b
riqsans <thrq88@gmail.com>
2024-04-01 14:37:42 +0700

    menuliskan cout nama kota

--- 100_Struct/100_Struct.cpp
+++ 100_Struct/100_Struct.cpp
@@ -26,7 +26,9 @@ int main() {
 		cout << "Alamat Mahasiswa : " << endl;
 		cout << "\t Nama Desa : ";
 		cin >> mhs[i].alamat.desa;
-		
+		cout << "\t Nama Kota : ";
+		cin >> mhs[i].alamat.kota;
+	
 
 	}
 }
