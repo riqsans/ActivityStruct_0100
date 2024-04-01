9811a3a02d1b6cf93fbd0335ce7148cfa1c0df95
riqsans <thrq88@gmail.com>
2024-04-01 15:10:49 +0700

    memperbaiki error

--- 100_Struct/100_Struct.cpp
+++ 100_Struct/100_Struct.cpp
@@ -15,3 +15,34 @@ struct Mahasiswa {
     string umur;
 };
 
+int main() {
+    Mahasiswa mhs[3];
+    for (int i = 0; i < 3; i++) {
+        cout << "Data ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
+
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        getline(cin, mhs[i].alamat.desa);
+        cout << "\t Nama Kota : ";
+        getline(cin, mhs[i].alamat.kota);
+
+        cout << "Umur Mahasiswa : ";
+        getline(cin, mhs[i].umur);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Desa : " << mhs[i].alamat.desa;
+        cout << "\n \t Kota : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
+        cout << endl;
+    }
+}
