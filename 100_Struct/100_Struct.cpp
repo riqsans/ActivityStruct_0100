248bf88825bd49f9db97e57e9dc539c50c49f410
riqsans <thrq88@gmail.com>
2024-04-01 13:44:03 +0700

    Add project files.

---
+++ 100_Struct/100_Struct.cpp
@@ -0,0 +1,18 @@
+// 100_Struct.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	string umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs, nim;
+	cout << "Nama Mahasiswa : ";
+}
