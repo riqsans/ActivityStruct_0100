9938d756ac4eee172bac412086505435cc859e59
riqsans <thrq88@gmail.com>
2024-04-01 15:08:50 +0700

    membubat fungsi main pada contoh kedua

--- 100_Struct/100_Struct.cpp
+++ 100_Struct/100_Struct.cpp
@@ -34,15 +34,5 @@ int main() {
         getline(cin, mhs[i].umur); 
     }
 
-    for (int i = 0; i < 3; i++) {
-        cout << endl;
-        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
-        cout << "\n NIM : " << mhs[i].nim;
-        cout << "\n Nama : " << mhs[i].nama;
-        cout << "\n Alamat : ";
-        cout << "\n \t Desa : " << mhs[i].alamat.desa;
-        cout << "\n \t Kota : " << mhs[i].alamat.kota;
-        cout << "\n Umur : " << mhs[i].umur;
-        cout << endl;
-    }
+    
 }
